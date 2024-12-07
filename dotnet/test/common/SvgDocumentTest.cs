f6aa829f574881bfb54c06f562ff787beac16e7a
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2024-12-07 21:45:51 +0300

    fill

--- dotnet/test/common/SvgDocumentTest.cs
+++ dotnet/test/common/SvgDocumentTest.cs
@@ -38,9 +38,9 @@ namespace OpenQA.Selenium
             driver.Url = svgTestPage;
             IWebElement rect = driver.FindElement(By.Id("rect"));
 
-            Assert.That(rect.GetAttribute("fill"), Is.EqualTo("blue"));
+            Assert.That(rect.GetDomAttribute("fill"), Is.EqualTo("blue"));
             rect.Click();
-            Assert.That(rect.GetAttribute("fill"), Is.EqualTo("green"));
+            Assert.That(rect.GetDomAttribute("fill"), Is.EqualTo("green"));
         }
 
         [Test]
@@ -54,9 +54,9 @@ namespace OpenQA.Selenium
             driver.Url = svgTestPage;
             IWebElement rect = driver.FindElement(By.Id("rect"));
 
-            Assert.That(rect.GetAttribute("fill"), Is.EqualTo("blue"));
+            Assert.That(rect.GetDomAttribute("fill"), Is.EqualTo("blue"));
             ((IJavaScriptExecutor)driver).ExecuteScript("document.getElementById('rect').setAttribute('fill', 'yellow');");
-            Assert.That(rect.GetAttribute("fill"), Is.EqualTo("yellow"));
+            Assert.That(rect.GetDomAttribute("fill"), Is.EqualTo("yellow"));
         }
     }
 }
