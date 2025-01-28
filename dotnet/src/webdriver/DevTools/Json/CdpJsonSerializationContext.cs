c17acccf729a7200234b01bd1e3a58090ed96ac7
Michael Render <render.michael@gmail.com>
2025-01-28 01:05:45 -0500

    Fix formatting

--- dotnet/src/webdriver/DevTools/Json/CdpJsonSerializationContext.cs
+++
@@ -1,8 +0,0 @@
-// using System.Text.Json.Serialization;
-
-// namespace OpenQA.Selenium.DevTools.Json;
-
-// [JsonSerializable(typeof(ICommand))]
-// [JsonSerializable(typeof(ICommandResponse<>))]
-// [JsonSourceGenerationOptions(Converters = [typeof(StringConverter)])]
-// internal sealed partial class CdpSerializationContext : JsonSerializerContext;
