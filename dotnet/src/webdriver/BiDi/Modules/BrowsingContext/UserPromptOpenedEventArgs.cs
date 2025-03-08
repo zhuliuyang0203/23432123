d8ea5f11aa987104ed713785c4ea2a1344450656
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2025-03-08 15:21:11 +0300

    Update UserPromptOpenedEventArgs.cs

--- dotnet/src/webdriver/BiDi/Modules/BrowsingContext/UserPromptOpenedEventArgs.cs
+++ dotnet/src/webdriver/BiDi/Modules/BrowsingContext/UserPromptOpenedEventArgs.cs
@@ -21,7 +21,7 @@ using System.Text.Json.Serialization;
 
 namespace OpenQA.Selenium.BiDi.Modules.BrowsingContext;
 
-public record UserPromptOpenedEventArgs(BiDi BiDi, BrowsingContext Context, UserPromptType Type, string Message)
+public record UserPromptOpenedEventArgs(BiDi BiDi, BrowsingContext Context, Session.UserPromptHandlerType Handler, UserPromptType Type, string Message)
     : BrowsingContextEventArgs(BiDi, Context)
 {
     [JsonInclude]
