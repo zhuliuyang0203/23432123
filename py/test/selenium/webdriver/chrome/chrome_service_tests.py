6e45ca58ecd9b57848576cd0b85b95b38c62e542
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-04-09 19:45:37 -0400

    [py] Use mock in test_updates_path_after_setting_env_variable

--- py/test/selenium/webdriver/chrome/chrome_service_tests.py
+++ py/test/selenium/webdriver/chrome/chrome_service_tests.py
@@ -18,6 +18,8 @@ import os
 import subprocess
 import time
 
+from unittest.mock import patch
+
 import pytest
 
 from selenium.common.exceptions import WebDriverException
@@ -125,8 +127,6 @@ class TestChromeDriverService:
         assert "chromedriver" in service.path
 
     def test_updates_path_after_setting_env_variable(self, service):
-        new_path = "/foo/bar"
-        os.environ["SE_CHROMEDRIVER"] = new_path
         service.executable_path = self.service_path  # Simulating the update
-
-        assert "chromedriver" in service.executable_path
+        with patch.dict("os.environ", {"SE_CHROMEDRIVER": "/foo/bar"}):
+            assert "chromedriver" in service.executable_path
