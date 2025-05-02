b0dff5660dfd5cd3622d005c6c430552890a9a8b
titusfortner <titus.fortner@gmail.com>
2025-05-02 13:17:49 -0700

    [build] allow GitHub Actions runner to use 4GB for JVM Heap

--- scripts/github-actions/ci-build.sh
+++ scripts/github-actions/ci-build.sh
@@ -4,6 +4,9 @@ set -eufo pipefail
 # We want to see what's going on
 set -x
 
+# Define heap size for GitHub Actions runner
+HEAP_SIZE="-Xmx4g"
+
 # Default to auto if no parameter is provided
 CACHE_RESULTS="auto"
 
@@ -17,6 +20,7 @@ fi
 # shellcheck disable=SC2046
 bazel test --config=rbe-ci --build_tests_only \
   --keep_going --flaky_test_attempts=2 \
+  --host_jvm_args=${HEAP_SIZE} \
   --cache_test_results=${CACHE_RESULTS} \
   //... -- $(cat .skipped-tests | tr '\n' ' ')
 
