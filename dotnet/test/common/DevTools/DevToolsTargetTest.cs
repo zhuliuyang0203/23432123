7ad8dff3fa88c943adb6d29bf03a2e6187fb6088
Diego Molina <diemol@gmail.com>
2025-05-22 18:03:06 +0200

    [dotnet] Running DevTools tests with 136

--- dotnet/test/common/DevTools/DevToolsTargetTest.cs
+++ dotnet/test/common/DevTools/DevToolsTargetTest.cs
@@ -25,12 +25,12 @@ using System.Threading.Tasks;
 
 namespace OpenQA.Selenium.DevTools;
 
-using CurrentCdpVersion = V137;
+using CurrentCdpVersion = V136;
 
 [TestFixture]
 public class DevToolsTargetTest : DevToolsTestFixture
 {
-    private int id = 137;
+    private int id = 136;
 
     [Test]
     [IgnoreBrowser(Selenium.Browser.IE, "IE does not support Chrome DevTools Protocol")]
