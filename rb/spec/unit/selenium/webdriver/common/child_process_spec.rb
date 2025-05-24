2d4107a1c496cb53520d0ae77d0201c2d6c7cf57
aguspe <agustin.pe94@gmail.com>
2025-05-24 19:39:02 +0200

    guard against windows

--- rb/spec/unit/selenium/webdriver/common/child_process_spec.rb
+++ rb/spec/unit/selenium/webdriver/common/child_process_spec.rb
@@ -21,7 +21,7 @@ require File.expand_path('../spec_helper', __dir__)
 
 module Selenium
   module WebDriver
-    describe ChildProcess do
+    describe ChildProcess, except: [{platform: :windows, reason: 'This is only for Unix platforms'}] do
       it 'does not raise an error when terminating a non-existent process' do
         process = described_class.new('sleep', '5')
         process.start
